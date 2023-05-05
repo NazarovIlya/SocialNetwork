f28e5b03e53274675d19344c9a5c061d1d600430
NazarovIlya <eliakohai@gmail.com>
2023-05-06 01:40:46 +0300

    test controller problem

---
+++ SocialNetwork/PresentationAPI/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[contoller]")]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public string[] Get()
+		{
+			return new string[] { "Hello", "world!" };
+		}
+	}
+}
