63ffa3dff8ce82b742dfe844327089924bb7bbb2
NazarovIlya <eliakohai@gmail.com>
2023-05-07 01:32:20 +0300

    activeness component realized

--- SocialNetwork/client/src/App.tsx
+++ SocialNetwork/client/src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [activeness, setActiveness] = useState();
+  useEffect(() => {
+    axios.get("http://localhost:11333/api/Activeness").then((Response) => {
+      console.log(Response);
+      setActiveness(Response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
