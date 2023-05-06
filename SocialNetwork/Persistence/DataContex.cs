6150eadfa1db68826f2201b48fe140eaef2d4e9c
NazarovIlya <eliakohai@gmail.com>
2023-05-06 03:48:12 +0300

    DataContex added

---
+++ SocialNetwork/Persistence/DataContex.cs
@@ -0,0 +1,11 @@
+﻿using BusinessDomain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+	public class DataContex : DbContext
+	{
+		public DataContex(DbContextOptions options) : base(options) { }
+		public DbSet<Activeness> Activities { get; set; }
+	}
+}
