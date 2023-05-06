6ab77984b86326ceb9e40d8890e4d72ce20fa880
NazarovIlya <eliakohai@gmail.com>
2023-05-07 01:54:52 +0300

    cors policy implemented

--- SocialNetwork/PresentationAPI/Program.cs
+++ SocialNetwork/PresentationAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Persistence;
 using Persistence.ExperimentalData;
 
@@ -14,6 +15,17 @@ builder.Services.AddDbContext<DataContext>(op =>
 	op.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("CorsAccessControlAllowOriginPolicy", policy =>
+	{
+		policy.AllowAnyMethod()
+		.AllowAnyHeader()
+		.WithOrigins(builder.Configuration["Client-host"]);
+	});
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -21,6 +33,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseCors("CorsAccessControlAllowOriginPolicy");
 
 using (var serviceScope = app.Services.CreateScope())
 {
