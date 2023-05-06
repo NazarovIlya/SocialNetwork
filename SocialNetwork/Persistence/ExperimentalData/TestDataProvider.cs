3d5d42b6dd8076f8d33eddbb61eca81a96da41f2
NazarovIlya <eliakohai@gmail.com>
2023-05-06 12:52:35 +0300

    test data

--- SocialNetwork/Persistence/ExperimentalData/TestDataProvider.cs
+++ SocialNetwork/Persistence/ExperimentalData/TestDataProvider.cs
@@ -1,38 +1,34 @@
-﻿using BusinessDomain;
-using BusinessDomain.Model;
+﻿using BusinessDomain.Model;
+using Persistance;
 
-namespace Persistence.ExperimentalData
+namespace BusinessDomain.ExperimentalData
 {
 	public class TestDataProvider
 	{
-		public static async Task Provide(DataContext context, int count = 30)
+		public static async Task Provider(DataContex contex, int count = 20)
 		{
-			if (context.Activities.Any()) return;
+			if (contex.Activities.Any()) return;
 
 			List<Activeness> activities = new List<Activeness>();
 
-			for (int i = 1; i <= count; i++)
+			for (int i = 1; i < count; i++)
 			{
-				int month = Random.Shared.Next(1, 6);
-				int category = Random.Shared.Next(1, 10);
-				int city = Random.Shared.Next(1, 10);
-				int location = Random.Shared.Next(1, 10);
+				int month = Random.Shared.Next(0, 12);
+				int category = Random.Shared.Next(0, 10);
+				int city = Random.Shared.Next(0, 10);
+				int location = Random.Shared.Next(0, 10);
 
-				activities.Add(
-					new Activeness
-					{
-						Title = $"Тестовая активность #{i}",
-						PointTime = DateTime.SpecifyKind(DateTime.Now.AddMonths(month), DateTimeKind.Utc),
-						Description = $"Описание #{month}",
-						Category = $"Категория #{category}",
-						City = $"Город #{city}",
-						Location = $"Место проведения #{location}",
-					}
-				);
+
+				activities.Add(new Activeness
+				{
+					Title = $"Тестовая активность #{i}",
+					PointTime = DateTime.SpecifyKind(DateTime.Now.AddMonths(month), DateTimeKind.Utc),
+					Description = $"Описание #{month}",
+					Category = $" Категория {category}",
+					City = $"Город #{city}",
+					Location = $"Место проведения #{location}",
+				});
 			}
-
-			await context.Activities.AddRangeAsync(activities);
-			await context.SaveChangesAsync();
 		}
 	}
 }
