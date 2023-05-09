677592665a3917da9f2f6449ade822eb57283b35
NazarovIlya <eliakohai@gmail.com>
2023-05-09 12:12:28 +0300

    client private view complited

--- SocialNetwork/client/src/App.tsx
+++ SocialNetwork/client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { Activeness, ActivenessItem } from "./ActivenessItem";
+import { Button, Header } from "semantic-ui-react";
 
 function App() {
   const [activeness, setActiveness] = useState([]);
@@ -12,15 +13,16 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {activeness.map((e: Activeness) => (
-          <div key={e.id}>
-            <label htmlFor="cdc"></label>
-            <ActivenessItem activenessItem={e} />
-          </div>
-        ))}
-      </header>
+    <div>
+      <Header as="h1">
+        <img src="images/logo.png" />
+        <label>Social Network project</label>
+      </Header>
+      {activeness.map((e: Activeness) => (
+        <div key={e.id}>
+          <ActivenessItem activenessItem={e} />
+        </div>
+      ))}
     </div>
   );
 }
