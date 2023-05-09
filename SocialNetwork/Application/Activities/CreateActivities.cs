012a8794682b33ce29b172f75b62ba180e60e678
NazarovIlya <eliakohai@gmail.com>
2023-05-09 22:19:57 +0300

    CRUD commands added

---
+++ SocialNetwork/Application/Activities/CreateActivities.cs
@@ -0,0 +1,30 @@
+﻿using BusinessDomain.Model;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+	public class CreateActivities
+	{
+		public class Command : IRequest
+		{
+			public Activeness Item { get; set; }
+		}
+
+		public class Handler : IRequestHandler<Command>
+		{
+			private readonly DataContext context;
+
+			public Handler(DataContext context)
+			{
+				this.context = context;
+			}
+			public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+			{
+				context.Activities.Add(request.Item);
+				await context.SaveChangesAsync();
+				return Unit.Value;
+			}
+		}
+	}
+}
---
+++ SocialNetwork/Application/Activities/EditActivities.cs
@@ -0,0 +1,38 @@
+﻿using BusinessDomain.Model;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+	public class EditActivities
+	{
+		public class Command : IRequest
+		{
+			public Activeness Item { get; set; }
+		}
+
+		public class Handler : IRequestHandler<Command>
+		{
+			private readonly DataContext context;
+
+			public Handler(DataContext context)
+			{
+				this.context = context;
+			}
+			public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+			{
+				var activity = await context.Activities
+					.FindAsync(request.Item.Id);
+
+				activity.Title = request.Item.Title ?? activity.Title;
+				activity.Category = request.Item.Category ?? activity.Category;
+				activity.Description = request.Item.Description ?? activity.Description;
+				activity.City = request.Item.City ?? activity.City;
+				activity.PointTime = request.Item.PointTime;
+				activity.Location = request.Item.Location ?? activity.Location;
+
+				return Unit.Value;
+			}
+		}
+	}
+}
