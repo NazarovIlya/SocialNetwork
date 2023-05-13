8223b6446eca0e2a46f9b01a9275399c9b5fce4a
NazarovIlya <eliakohai@gmail.com>
2023-05-13 13:49:19 +0300

    using AutoMapper for edit activities & fixed a bug(+ context.SaveChagesAsync()

--- SocialNetwork/Application/Activities/EditActivities.cs
+++ SocialNetwork/Application/Activities/EditActivities.cs
@@ -1,4 +1,5 @@
-﻿using BusinessDomain.Model;
+﻿using AutoMapper;
+using BusinessDomain.Model;
 using MediatR;
 using Persistence;
 
@@ -14,22 +15,20 @@ namespace Application.Activities
 		public class Handler : IRequestHandler<Command>
 		{
 			private readonly DataContext context;
+			private readonly IMapper mapper;
 
-			public Handler(DataContext context)
+			public Handler(DataContext context, IMapper mapper)
 			{
 				this.context = context;
+				this.mapper = mapper;
 			}
 			public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
 			{
 				var activity = await context.Activities
 					.FindAsync(request.Item.Id);
 
-				activity.Title = request.Item.Title ?? activity.Title;
-				activity.Category = request.Item.Category ?? activity.Category;
-				activity.Description = request.Item.Description ?? activity.Description;
-				activity.City = request.Item.City ?? activity.City;
-				activity.PointTime = request.Item.PointTime;
-				activity.Location = request.Item.Location ?? activity.Location;
+				mapper.Map(request.Item, activity);
+				await context.SaveChangesAsync();
 
 				return Unit.Value;
 			}
